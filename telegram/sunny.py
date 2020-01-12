cf207ae00d6eba024c894668c39346979ab47d5b
Soner Abay <11530785+SonerAbay@users.noreply.github.com>
2020-01-13 01:35:32 +0300

    Update sunny.py

--- telegram/sunny.py
+++ telegram/sunny.py
@@ -86,6 +86,24 @@ def help():
 
 	return "<b>Command List:</b>\n\n<b>/line</b> - Enter a line from the show to find the episode.\n<b>Usage:</b>/line your line\n\n<b>/episodes</b> - Get the list of episodes from a season.\n<b>Usage:</b> /episodes season_number\n\n<b>/random</b> - Get a random episode from the show.\n<b>Usage:</b> /random"
 
+def trivia():
+	
+	trivia = random.randint(1,60)
+	conn = None
+	try:
+		conn = sqlite3.connect("/mnt/c/Users/soner/Documents/MYGITHUB/SUNNY/chinook/subtitles.db")
+	except sqlite3.Error as e:
+		print(e)
+
+	cur = conn.cursor()
+	sql = "SELECT text FROM trivia WHERE id = {}".format(trivia)
+
+	cur.execute(sql)
+
+	result = cur.fetchall()
+
+	return "<b>Did you know?</b>\n\n" + result[0][0]
+
 def text(input_text):
 	text, command = group_split(input_text)
 	if command == '/line':
@@ -96,4 +114,6 @@ def text(input_text):
 		return random_episode()
 	if command == '/sunny_help':
 		return help()
+	if command == '/trivia':
+		return trivia()	
 		
